d7be32ece68f3251813a30e8be25075a909753ab
wenxiaolan <wenxiaolan02@outlook.com>
2024-05-22 21:47:08 +0800

    项目创建

---
+++ src/main/java/cn/lulucar/springbootshirovue/SpringbootShiroVueApplication.java
@@ -0,0 +1,13 @@
+package cn.lulucar.springbootshirovue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootShiroVueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootShiroVueApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/lulucar/springbootshirovue/SpringbootShiroVueApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.lulucar.springbootshirovue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootShiroVueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
